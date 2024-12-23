3bae59036ac345dfffabaac0a2d44159bee22dcd
Inzamam <inzamaminbox@gmail.com>
2024-12-23 09:55:53 +0000

    first python from local

---
+++ HelloSpark.py
@@ -0,0 +1,14 @@
+from pyspark.sql import *
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder \
+    .appName("Hello Spark") \
+    .master("local[2]") \
+    .getOrCreate()
+
+    data_list = [
+        ("Ravi", 28),
+        ("David", 45),
+        ("Abdul", 37)
+    ]
