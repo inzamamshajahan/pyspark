3417478c9736e1da7f7653fa71d85adbd387066e
inzamam <inzamaminbox@gmail.com>
2024-12-28 13:11:04 +0000

    added a function to read a csv file

--- HelloSpark.py
+++ HelloSpark.py
@@ -1,15 +1,27 @@
 from pyspark.sql import *
-from lib import logger as lg
+from lib.logger import *
+from lib.utils import *
+import sys
+
+
 
 if __name__ == "__main__":
-
+    conf = get_spark_app_config()
     spark = SparkSession.builder \
-    .appName("Hello Spark") \
-    .master("local[3]") \
+    .config(conf=conf) \
     .getOrCreate()
 
-    logger = lg.Log4j(spark)
+    logger = Log4j(spark)
     logger.info("Starting HelloSpark")
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
     # Processing code
+    # conf_out = spark.sparkContext.getConf()
+    # logger.info(conf_out.toDebugString())
+    survey_df = load_survey_df(spark, sys.argv[1])
+    survey_df.show()
+    
     logger.info("Finished HelloSpark")
     # spark.stop()
