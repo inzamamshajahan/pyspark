295b7278de1e3c2aa6d47c4c9c68998997e7866c
inzamam <inzamaminbox@gmail.com>
2024-12-29 12:53:46 +0000

    added a function for dataframe transformations

--- HelloSpark.py
+++ HelloSpark.py
@@ -21,7 +21,17 @@ if __name__ == "__main__":
     # conf_out = spark.sparkContext.getConf()
     # logger.info(conf_out.toDebugString())
     survey_df = load_survey_df(spark, sys.argv[1])
-    survey_df.show()
-    
+    partitioned_survey_df = survey_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+
+    logger.info(count_df.collect())
+    # In the above code we used collect() instead of show(), because
+    # The collect() action returnes the result as a python list
+    # The show() method is a utiity function to print the dataframe, usually used for debugging purposes
+
+    input("Press Enter")
+    # The above input ensures that our program do not finish so that we can analyze the execution plan via spark UI
+    # While a pyspark program is running localhost:4040 shows the execution plan
+
     logger.info("Finished HelloSpark")
     # spark.stop()
--- lib/utils.py
+++ lib/utils.py
@@ -16,3 +16,13 @@ def load_survey_df(spark, data_file):
                  .option("header", "true") \
                  .option("inferSchema", "true") \
                  .csv(sys.argv[1])
+    # in the above code read is one action
+    # inferSchema is another action
+    # so this function will lead to 2 jobs in spark UI, because it is 2 actions.
+
+def count_by_country(survey_df):
+    return survey_df \
+    .where("Age < 40") \
+    .select("Age", "Gender", "Country", "state") \
+    .groupBy("Country") \
+    .count()
