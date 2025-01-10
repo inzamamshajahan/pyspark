4b9e3fad95ade877d4dd5c7e6e3143b5c2e87b1e
inzamam <inzamaminbox@gmail.com>
2025-01-10 14:20:19 +0000

    explored the ways to specify schema while loading data

--- 104-SparkSchemaDemo/SparkSchemaDemo.py
+++ 104-SparkSchemaDemo/SparkSchemaDemo.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from lib.logger import Log4j
+from pyspark.sql.types import StructType, StructField, DateType, StringType, IntegerType
 
 if __name__ == "__main__":
     spark = SparkSession \
@@ -16,13 +17,61 @@ if __name__ == "__main__":
     # .load("data/flight*.csv")
     # # in the above method of reading the DF all the columns will be given a string data type
 
+    # flightTimeCsvDF = spark.read \
+    #     .format('csv') \
+    #     .option("header", "true") \
+    #     .option("inferSchema", "true") \
+    #     .load("data/flight*.csv")
+    # # in the above method the schema will be infered, that is not all the columns will be string.
+
+    # flightTimeCsvDF.show(5)
+    # logger.info("CSV Schema:" + flightTimeCsvDF.schema.simpleString())
+    
+    flightSchemaStruct = StructType([
+        StructField("FL_DATE", DateType()),
+        StructField("OP_CARRIER", StringType()),
+        StructField("OP_CARRIER_FL_NUM", IntegerType()),
+        StructField("ORIGIN", StringType()),
+        StructField("ORIGIN_CITY_NAME", StringType()),
+        StructField("DEST", StringType()),
+        StructField("DEST_CITY_NAME", StringType()),
+        StructField("CRS_DEP_TIME", IntegerType()),
+        StructField("DEP_TIME", IntegerType()),
+        StructField("WHEELS_ON", IntegerType()),
+        StructField("TAXI_IN", IntegerType()),
+        StructField("CRS_ARR_TIME", IntegerType()),
+        StructField("ARR_TIME", IntegerType()),
+        StructField("CANCELLED", IntegerType()),
+        StructField("DISTANCE", IntegerType())
+    ])
+    
     flightTimeCsvDF = spark.read \
         .format('csv') \
         .option("header", "true") \
-        .option("inferSchema", "true") \
+        .schema(flightSchemaStruct) \
+        .option("mode", "FAILFAST") \
+        .option("dateFormat", "M/d/y") \
         .load("data/flight*.csv")
-    # in the above method the schema will be infered, that is not all the columns will be string.
+    # if the data type specified in the schema object do not match with the data being loaded, pyspark will throw a runtime error.
+    # when we specify the schema we should also specify the dateformat
+    
 
+    flightTimeCsvDF.show(5)
+    logger.info("CSV Schema:" + flightTimeCsvDF.schema.simpleString())
+    
+    #Below is the DDL method to specify the schema
+    flightSchemaDDL = """FL_DATE DATE, OP_CARRIER STRING, OP_CARRIER_FL_NUM INT, ORIGIN STRING, 
+          ORIGIN_CITY_NAME STRING, DEST STRING, DEST_CITY_NAME STRING, CRS_DEP_TIME INT, DEP_TIME INT, 
+          WHEELS_ON INT, TAXI_IN INT, CRS_ARR_TIME INT, ARR_TIME INT, CANCELLED INT, DISTANCE INT"""
+          
+    flightTimeCsvDF = spark.read \
+        .format('csv') \
+        .option("header", "true") \
+        .schema(flightSchemaDDL) \
+        .option("mode", "FAILFAST") \
+        .option("dateFormat", "M/d/y") \
+        .load("data/flight*.csv")     
+    
     flightTimeCsvDF.show(5)
     logger.info("CSV Schema:" + flightTimeCsvDF.schema.simpleString())
 
