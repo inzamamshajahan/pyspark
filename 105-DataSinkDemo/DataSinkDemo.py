7eb69b5fd90c5f6a51bc46bfa703c47e619ad7f1
inzamam <inzamaminbox@gmail.com>
2025-01-11 09:12:11 +0000

    added config to add a new spark jar

---
+++ 105-DataSinkDemo/DataSinkDemo.py
@@ -0,0 +1,6 @@
+# in this example we are going to generate spark avro output.
+# Spark avro do not come bundled with apache spark
+# so to work with avro data we need to include and additional scala package
+# we are working of python ide and it do not add option to add any java or scala dependencies
+# added it in the spark-default.conf file
+
